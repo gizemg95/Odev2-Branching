6dc5e1c86d356304d4dd7c43b46df9ddcdbf2898
Gizem Gorgun Goksu <gizemgorgun15@gmail.com>
2023-03-28 22:11:47 +0300

    coin toplandıkça ses çalma ile ilgili fonksiyon eklendi

--- coin.cs
+++ coin.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class CoinCollection : MonoBehaviour
 {
+    private AudioSource click;
+
+    private void Start()
+    {
+        click = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin"))
         {
             Destroy(other.gameObject);
+            click.Play();
         }
     }
 }
